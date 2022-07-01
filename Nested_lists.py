0e79b3dc5a6498ed72b86a28e7a21b19253d0d14
Dharmendra Choudhary <dnc.1124@gmail.com>
2022-07-01 18:44:32 +0530

    Update Nested_lists.py

--- Nested_lists.py
+++ Nested_lists.py
@@ -13,3 +13,17 @@ if __name__ == '__main__':
     score_list=list()
     for i in range(0,len(l)):
         score_list.append(l[i][1])
+    print(score_list)
+    score_list=sorted(score_list,reverse=True)
+    final_list=[]
+    for i in range(0,len(l)):
+        if score_list[-1]==l[i][1]:
+            final_list.append(l[0][i])
+        elif score_list[-2]==l[i][1]:
+            final_list.append(l[0][i])
+        else:
+            pass
+   for i in sorted(score_list):
+       print(i)
+    
+        
