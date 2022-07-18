2fa839707dafb47b3ab6821e1f9bfe9a0db173b9
Dharmendra Choudhary <dnc.1124@gmail.com>
2022-07-18 18:50:47 +0530

    Create itertools_combinations.py

---
+++ itertools_combinations.py
@@ -0,0 +1,8 @@
+## question link : https://www.hackerrank.com/challenges/itertools-combinations/problem
+
+from itertools import combinations
+string, n = input().split()
+for i in range(1,int(n)+1):
+    data = ["".join(sorted(i)) for i in combinations(string,i)]
+    data.sort()
+    print("\n".join(data))
